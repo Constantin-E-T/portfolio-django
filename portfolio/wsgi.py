558f03efdb62baaa48730cbde400253c51044d92
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-07-07 02:11:36 +0300

    styling2

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -3,9 +3,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise  # new
+from django.conf import settings  # new
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=str(BASE_DIR / 'staticfiles'))  # new
-
+application = WhiteNoise(application, root=str(settings.BASE_DIR / 'staticfiles'))  # modified
