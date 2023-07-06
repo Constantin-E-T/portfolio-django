038758cfa506f786f67355a5715447d257fbf2e0
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-07-06 19:49:04 +0300

    the app is deployd, but i have no styling. now i sort the admin page

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for portfolio project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
+# portfolio/wsgi.py
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
+
+# Collect static files
+if not os.getenv('DEBUG', 'True') == 'True':
+    from django.core.management import call_command
+    call_command('collectstatic', '--noinput')
