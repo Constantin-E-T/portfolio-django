fa8855caf32b5850d2500c4c80d8a5500c6ae848
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-07-10 00:59:25 +0300

    added DB

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,11 +76,19 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
+# AWS postgresDB
+
 DATABASES = {
-'default': dj_database_url.config(
-    default=config('DATABASE_URL')
-)
+    'default': {
+        'ENGINE':config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
 }
+
 
 
 # DATABASES = {
