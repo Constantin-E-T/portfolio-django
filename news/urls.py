23ca4fec8155146c19c529ec40a53219f2f30a77
Emilian Con <emiliancon@Emilians-MBP-2.home>
2023-08-09 15:20:44 +0100

    improve filtering by tags or category

--- news/urls.py
+++ news/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.news_list, name='news_list'),
     path('category/<slug:category_slug>/', views.news_list, name='news_list_by_category'),
-    path('<int:id>/<slug:slug>/', views.news_detail, name='news_detail'),
     path('tag/<slug:tag_slug>/', views.news_by_tag, name='news_by_tag'),
+    path('news/<int:id>/<slug:slug>/', views.news_detail, name='news_detail'),
 ]
--- news/views.py
+++ news/views.py
@@ -7,14 +7,14 @@ from django.core.paginator import Paginator
 def news_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    news_list = News.objects.all()
+    news_list = News.objects.all().order_by('-created_at')
     latest_news = News.objects.order_by('-created_at')[:5]  # Get the 5 latest news articles
     tags = Tag.objects.all()  # Get all tags
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         news_list = news_list.filter(category=category)
     
-    paginator = Paginator(news_list, 1)  # Show 5 news per page
+    paginator = Paginator(news_list, 3)  # Show 5 news per page
     page_number = request.GET.get('page')
     news = paginator.get_page(page_number)
 
@@ -29,8 +29,24 @@ def news_detail(request, id, slug):
 
 def news_by_tag(request, tag_slug):
     tag = get_object_or_404(Tag, slug=tag_slug)
-    news = News.objects.filter(tags=tag)
-    return render(request, 'news/news_list.html', {'news': news})
+    news_list = News.objects.filter(tags=tag).order_by('-created_at')
+    categories = Category.objects.all()
+    latest_news = News.objects.order_by('-created_at')[:5]
+    tags = Tag.objects.all()
+
+    paginator = Paginator(news_list, 3)  # Show 1 news per page for demonstration
+    page_number = request.GET.get('page')
+    news = paginator.get_page(page_number)
+
+    context = {
+        'news': news,
+        'categories': categories,
+        'latest_news': latest_news,
+        'tags': tags
+    }
+    return render(request, 'news/news_list.html', context)
+
+
 
 
 
