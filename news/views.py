acdd648a7133354331e75fb7ad400982f85a8e6c
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-08-10 11:48:20 +0100

    new updates to the news pages

--- news/views.py
+++ news/views.py
@@ -2,19 +2,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import News, Category, Tag
 from django.core.paginator import Paginator
+from django.db.models import Count
+
 
 
 def news_list(request, category_slug=None):
     category = None
-    categories = Category.objects.all()
+    # Fetch only categories that have associated posts
+    categories = Category.objects.annotate(num_posts=Count('news')).filter(num_posts__gt=0)
+
     news_list = News.objects.all().order_by('-created_at')
     latest_news = News.objects.order_by('-created_at')[:5]  # Get the 5 latest news articles
-    tags = Tag.objects.all()  # Get all tags
+    # Fetch only tags that have associated posts
+    tags = Tag.objects.annotate(num_news=Count('news__id')).filter(num_news__gt=0)
+
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         news_list = news_list.filter(category=category)
     
-    paginator = Paginator(news_list, 3)  # Show 5 news per page
+    paginator = Paginator(news_list, 2)  # Show 5 news per page
     page_number = request.GET.get('page')
     news = paginator.get_page(page_number)
 
@@ -30,11 +36,13 @@ def news_detail(request, id, slug):
 def news_by_tag(request, tag_slug):
     tag = get_object_or_404(Tag, slug=tag_slug)
     news_list = News.objects.filter(tags=tag).order_by('-created_at')
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(num_posts=Count('news')).filter(num_posts__gt=0)
     latest_news = News.objects.order_by('-created_at')[:5]
-    tags = Tag.objects.all()
 
-    paginator = Paginator(news_list, 3)  # Show 1 news per page for demonstration
+    # Fetch only tags that have associated posts
+    tags = Tag.objects.annotate(num_news=Count('news__id')).filter(num_news__gt=0)
+
+    paginator = Paginator(news_list, 3)  # Show 3 news per page for demonstration
     page_number = request.GET.get('page')
     news = paginator.get_page(page_number)
 
@@ -42,9 +50,11 @@ def news_by_tag(request, tag_slug):
         'news': news,
         'categories': categories,
         'latest_news': latest_news,
-        'tags': tags
+        'tags': tags,
+        'selected_tag': tag
     }
     return render(request, 'news/news_list.html', context)
+
 
 
 
